0322f517d971ef9298fe65d3887990541c859f53
Harsha Bashetty <harshav05@harshas-mbp.home>
2022-06-03 11:41:41 +0100

    db connection and loading data

--- main.py
+++ main.py
@@ -14,3 +14,24 @@ df = df.reset_index(level=0)
 df.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
 
 print(df.head())
+
+# Import dataframe into MySQL
+import sqlalchemy
+database_username = 'root'
+database_password = 'password'
+database_ip       = '127.0.0.1'
+database_name     = 'stock_data'
+database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
+                                               format(database_username, database_password, 
+                                                      database_ip, database_name))
+df.to_sql(con=database_connection, name='reliance_data', if_exists='replace')
+
+
+sql_query = sqlalchemy.text("SELECT * FROM reliance_data")
+result = database_connection.execute(sql_query)
+result_as_list = result.fetchall()
+print('------------------')
+print('Table: Reliance Data')
+print('------------------')
+for row in result_as_list:
+    print(row)
